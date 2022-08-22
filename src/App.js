63cf02e29d2c7fb0036c82cae426d4adbca44b1e
Emmanuel <emmanuelqkorir@gmail.com>
2022-08-22 17:07:01 +0300

    add header section and material UI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     //BEM naming convention
     <div className="app">
-      <h1>Hello world</h1>
+      <Header />
+      {/* {Home} */}
     </div>
   );
 }
