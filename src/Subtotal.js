94d187553dfd961fdac0cb228af3aa25e2b9c2d9
Emmanuel <emmanuelqkorir@gmail.com>
2022-08-23 15:26:15 +0300

    add selector

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Subtotal.css';
 import CurrencyFormat from 'react-currency-format';
 import { useStateValue } from './StateProvider';
+import { getBasketTotal } from './reducer';
 
 function Subtotal() {
   const [{ basket }, dispatch] = useStateValue();
@@ -11,7 +12,8 @@ function Subtotal() {
         renderText={(value) => (
           <div>
             <p>
-              Subtotal({basket?.length} items): <strong>0</strong>
+              Subtotal({basket?.length} items):{' '}
+              <strong>{value}</strong>
             </p>
             <small className="subtotal__gift">
               <input type="checkbox" /> This order contains a gift
@@ -19,7 +21,7 @@ function Subtotal() {
           </div>
         )}
         decimalScale={2}
-        value={0}
+        value={getBasketTotal(basket)}
         displayType={'text'}
         thousandSeparator={true}
         prefix={'$'}
