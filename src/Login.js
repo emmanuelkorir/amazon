98d66c462fc95689f34a935957ea29b140ca3019
Emmanuel <emmanuelqkorir@gmail.com>
2022-08-24 13:04:44 +0300

    add usestate hooks

--- src/Login.js
+++ src/Login.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-
 import './Login.css';
 
 function Login() {
+  const [email, setemail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const signIn = (e) => {
+    e.preventDefault();
+  };
+
+  const register = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="login">
       <Link to="/">
@@ -19,12 +29,24 @@ function Login() {
 
         <form>
           <h5>E-mail</h5>
-          <input type="text" />
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
 
           <h5>Password</h5>
-          <input type="password" />
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
 
-          <button type="submit" className="login__signInButton">
+          <button
+            type="submit"
+            onClick={signIn}
+            className="login__signInButton"
+          >
             Sign In
           </button>
         </form>
@@ -34,7 +56,7 @@ function Login() {
           Privacy Notice.
         </p>
 
-        <button className="login__registerButton">
+        <button onClick={register} className="login__registerButton">
           Create your Amazon Account
         </button>
       </div>
